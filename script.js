73297ccc3de2832b86e85b9acb4ae09ee7549d6e
Vishal Jaiswada <vp022570@gmail.com>
2024-08-13 20:41:20 +0530

    Completed 100% html , 100% css and 90% js code

---
+++ script.js
@@ -0,0 +1,50 @@
+const API_KEY ="81eb73320c5f5944dd6bef649712ae92";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function getWeather(city){
+      
+            // const city = "greater noida";
+            const response =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+            var data = await response.json();
+            
+             if(response.status === 404){
+                document.querySelector(".error").style.display ="block";
+                document.querySelector(".weather").style.display ="none";
+             }else{
+
+                console.log(data);
+
+                document.querySelector(".city").innerHTML = `${data.name}` ;
+                document.querySelector(".temp").innerHTML = `${Math.round(data.main.temp)}°c`;
+                document.querySelector(".humidity").innerHTML = `${data.main.humidity}%`;
+                document.querySelector(".wind").innerHTML = `${data.wind.speed}km/h`;
+    
+                if(data.weather[0].main == "Clouds"){
+                    weatherIcon.src = "images/clouds.png" ;
+                }
+                else if(data.weather[0].main == "Clear"){
+                    weatherIcon.src = "images/clear.png" ;
+                }
+                else if(data.weather[0].main == "Rain"){
+                    weatherIcon.src = "images/rain.png" ;
+                }
+                else if(data.weather[0].main == "Drizzle"){
+                    weatherIcon.src = "images/drizzle.png" ;
+                }
+                else if(data.weather[0].main == "Mist" || data.weather[0].main == "Haze" ){
+                    weatherIcon.src = "images/mist.png" ;
+                }
+                document.querySelector(".error").style.display ="none";
+                document.querySelector(".weather").style.display ="block";
+             }             
+        
+}
+
+ searchBtn.addEventListener("click",()=>{
+    const city = searchBox.value;
+    getWeather(city);
+ })
+
