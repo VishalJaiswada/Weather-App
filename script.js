4f4a8c8f8f530e1a9e78ce6235925fe02729f4f4
Vishal Jaiswada <vp022570@gmail.com>
2024-08-13 23:04:44 +0530

    100% completed javascript code

--- script.js
+++ script.js
@@ -48,3 +48,47 @@ async function getWeather(city){
     getWeather(city);
  })
 
+
+ function getCurrentLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showCurrentPosition);
+    }
+ }
+
+ async function showCurrentPosition(position){
+
+                let lat = position.coords.latitude;
+                let lon = position.coords.longitude;
+                //console.log(lat,lon);
+                
+                
+
+                const response =await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+                var data = await response.json();
+                
+                console.log(data);
+                
+                document.querySelector(".city").innerHTML = `${data.name}` ;
+                document.querySelector(".temp").innerHTML = `${Math.round(data.main.temp)}°c`;
+                document.querySelector(".humidity").innerHTML = `${data.main.humidity}%`;
+                document.querySelector(".wind").innerHTML = `${data.wind.speed}km/h`;
+    
+                if(data.weather[0].main == "Clouds"){
+                    weatherIcon.src = "images/clouds.png" ;
+                }
+                else if(data.weather[0].main == "Clear"){
+                    weatherIcon.src = "images/clear.png" ;
+                }
+                else if(data.weather[0].main == "Rain"){
+                    weatherIcon.src = "images/rain.png" ;
+                }
+                else if(data.weather[0].main == "Drizzle"){
+                    weatherIcon.src = "images/drizzle.png" ;
+                }
+                else if(data.weather[0].main == "Mist" || data.weather[0].main == "Haze" ){
+                    weatherIcon.src = "images/mist.png" ;
+                }
+                
+ }
+
+getCurrentLocation();
